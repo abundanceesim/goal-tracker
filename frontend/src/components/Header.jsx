5c5790182e108a0334bf2d97e6eaaa90e4f2fa83
Abundance Esim <abundanceesim@gmail.com>
2023-12-27 16:00:18 -0500

    Modify favicon

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -20,7 +20,7 @@ function Header() {
   return (
     <header className="header">
       <div className="logo">
-        <Link to="/">GoalSetter</Link>
+        <Link to="/">Goal Tracker</Link>
       </div>
       <ul>
         {user ? (
